74cff2e3d77d520b7b91e2570cdfde70c1a1558a
Michel Grootjans <michel.grootjans@gmail.com>
2026-05-09 15:01:50 +0200

    set build target to es2022 in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/interclub-opstelling/',
+  build: {
+    target: 'es2022',
+  },
   test: {
     environment: 'node',
   },
